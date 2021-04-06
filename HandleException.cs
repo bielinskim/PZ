92d977b0ad6d7fc7e81390649ea3f496255d350a
Mateusz Bieliñski <mateusz100022@gmail.com>
2021-04-06 20:22:30 +0200

    Add project files.

---
+++ HandleException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03
+{
+    static class HandleException
+    {
+        
+        static public void Handle(Exception ex)
+        {
+            ConsoleColor origialConsoleColor = System.Console.ForegroundColor;
+            System.Console.ForegroundColor = ConsoleColor.Red;
+            if (ex is ApplicationException)
+            {
+                Console.WriteLine("Wyjątek aplikacyjny");
+            } else
+            {
+                Console.WriteLine("Wyjątek systemowy");
+            }
+            Console.WriteLine($"Pełna nazwa kwalifikowana: {ex.GetType()}");
+            Console.WriteLine($"Message: {ex.Message}");
+            Console.WriteLine($"TargetSite: {ex.TargetSite}");
+            Console.WriteLine($"StackTrace: {ex.StackTrace}");
+            Console.WriteLine($"HelpLink: {ex.HelpLink}");
+            Console.WriteLine($"Data IsFixedSize: {ex.Data.IsFixedSize}");
+            Console.WriteLine($"Data IsReadOnly: {ex.Data.IsReadOnly}");
+            Console.WriteLine($"Data IsSynchronized: {ex.Data.IsSynchronized}");
+            System.Console.ForegroundColor = origialConsoleColor;
+
+            if(ex.InnerException != null)
+            {
+                Handle(ex.InnerException);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Lab03Library;
+
+namespace Lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+            DocumentManager dm = new DocumentManager();
+
+            dm.Add(new Volume(8370541534, "Narrenturm", 2002, 588, "Andrzej Sapkowski", 1, 3));
+            dm.Add(new Volume(9788375781403, "Boży bojownicy", 2004, 640, "Andrzej Sapkowski", 2, 3));
+            dm.Add(new Volume(9788375781410, "Lux perpetua", 2006, 608, "Andrzej Sapkowski", 3, 3));
+            dm.Add(new Book(8373272267, "Lalka", 2021, 668, "Bolesław Prus"));
+            dm.Add(new Book(9788373272248, "Chłopi", 2020, 718, "Władysław Reymont"));
+            dm.Add(new Magazine(23917962, "CD-Action", 2021, 148, 319, Frequencies.Monthly));
+            dm.Add(new Magazine(63162905, "Świat Wiedzy", 2021, 112, 211, Frequencies.Monthly));
+
+            dm.Add(new Volume(9788375781410, "Lux perpetua", 2006, 608, "Andrzej Sapkowski", 3, 3));
+            //dm.Add(new Volume(3242424, "Boży bojownicy", 2004, 640, "Andrzej Sapkowski", 4, 3));
+            Console.WriteLine("===========Lista wszystkich==============");
+            dm.ListToConsole(dm.Documents);
+            Console.WriteLine("===========GetByISBN==============");
+            Console.WriteLine(dm.GetByISBN(8370541534));
+            Console.WriteLine("===========GetByPhraseInTitle==============");
+            var filteredList = dm.GetByPhraseInTitle("Świat");
+            dm.ListToConsole(filteredList);
+            Console.WriteLine("===========GetByFrequency==============");
+            filteredList = dm.GetByFrequency(Frequencies.Monthly);
+            dm.ListToConsole(filteredList);
+            Console.WriteLine("===========GetVolumesByTitle==============");
+            filteredList = dm.GetVolumesByTitle("Narrenturm");
+            dm.ListToConsole(filteredList);
+            Console.WriteLine("===========Drukowanie publikacji==============");
+            Console.WriteLine(dm.Documents[0].Print());
+
+            }
+            catch (Exception ex)
+            {
+                HandleException.Handle(ex);
+            }
+        }
+    }
+}
